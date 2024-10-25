6ecd06231ef4429bd38bec6b32c25ebd816afcd1
jexpl12 <kolekmemilihmurtad@gmail.com>
2024-10-25 10:06:02 +0700

    feat: [BW] styling-footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,15 +7,15 @@ import twitter from '../public/IcTwitter.svg';
 
 const Footer = () => {
   return (
-    <div className='bg-[#0F0F0F] mx-auto px-0 2xl:px-4'>
-        <div className="container">
-            <div className='flex justify-between items-center'>
-                <div> webdevindo©Copyright 2023</div>
-                <div className="socmed flex justify-end items-center gap-0">
-                    <a href="#"><Image src={linkedin} alt="linkedin"/></a>
+    <div className='bg-[#0F0F0F]'>
+        <div className="container mx-auto px-4 2xl:px-0">
+            <div className='flex flex-col justify-center items-center md:flex-row md:justify-between gap-[15px] py-[50px]'>
+              <div>Kuanta 2023</div>
+                <div className="socmed flex justify-end items-center gap-[12px]">
+                    <a href="#"><Image src={facebook} alt="fb"/></a>
                     <a href="#"><Image src={telegran} alt="telegram"/></a>
                     <a href="#"><Image src={twitter} alt="twitter"/></a>
-                    <a href="#"><Image src={facebook} alt="fb"/></a>
+                    <a href="#"><Image src={linkedin} alt="linkedin"/></a>
                 </div>
             </div>
         </div>
