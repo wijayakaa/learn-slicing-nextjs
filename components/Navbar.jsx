4f4de1519a4ffe48f434fbedbd24d35d5ff0716a
jexpl12 <kolekmemilihmurtad@gmail.com>
2024-10-25 14:11:50 +0700

    style: [BW] make-about-page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,39 +11,42 @@ const Navbar = () => {
   const currentPage = usePathname();
   console.log(currentPage);
   const [show, setShow] = useState(false)
+
   return (
-    <div className="container mx-auto px-4 2xl:px-0">
-      <div className='py-4  flex justify-between items-center'>
-        <Image src={Logo} alt='Logo' priority />
+    <section className={`${currentPage !== '/' ? 'border-b-[1px] border-white' : ''}`}>
+        <div className="container mx-auto px-4 2xl:px-0">
+          <div className='py-10  flex justify-between items-center'>
+            <Image src={Logo} alt='Logo' priority />
 
-        <HiOutlineMenuAlt3
-        className='text-white cursor-pointer lg:hidden' 
-        size={30} 
-        onClick={(prev)=>setShow(!prev.show)}
-        />
-          
-        <div className={`fixed lg:relative bg-black lg:bg-transparent top-0 w-full h-screen lg:h-auto ${show ? 'right-0' : '-right-full'} lg:right-0 flex gap-10 flex-col lg:flex-row justify-center lg:justify-end items-center transition-all duration-300 top_nav`}>
-          
-          <HiOutlineX
-          className='text-white absolute top-7 right-7 cursor-pointer lg:hidden' 
-          size={30}
-          onClick={(prev)=>setShow(prev.show)}
-          />
+            <HiOutlineMenuAlt3
+              className='text-white cursor-pointer lg:hidden'
+              size={30}
+              onClick={(prev) => setShow(!prev.show)}
+            />
 
-          {menu.map((item,i) => {
-              return (
+            <div className={`fixed lg:relative bg-black lg:bg-transparent top-0 w-full h-screen lg:h-auto ${show ? 'right-0' : '-right-full'} lg:right-0 flex gap-10 flex-col lg:flex-row justify-center lg:justify-end items-center transition-all duration-300 top_nav`}>
+
+              <HiOutlineX
+                className='text-white absolute top-7 right-7 cursor-pointer lg:hidden'
+                size={30}
+                onClick={(prev) => setShow(prev.show)}
+              />
+
+              {menu.map((item, i) => {
+                return (
                   <Link
-                  className={currentPage == item.url? "active" : ""} 
-                  key={i}
-                  href={item.url}>
+                    className={currentPage == item.url ? "active" : ""}
+                    key={i}
+                    href={item.url}>
                     {item.label}
                   </Link>
-              )
-          })}
+                )
+              })}
 
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
   )
 }
 
