f098857aca3dc441ce27491d8f02f8396f23f681
jexpl12 <kolekmemilihmurtad@gmail.com>
2024-10-27 15:41:02 +0700

    feat: [BW] make-blog-page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import banner from '../../public/about/banner.png';
+import { Poppins } from 'next/font/google';
+import Link from 'next/link';
+import { blog } from '@/data/blog';
 
+
+const poppins  = Poppins({subsets: ['latin'], weight: "600"});
 const Blog = () => {
   return (
-    <div>
-      Blog
-    </div>
+    <section>
+      <div>
+        <div className="banner-page">
+          <Image src={banner} alt="banner" className='w-full' />
+        </div>
+        <div className="container mx-auto px px-4 py-16 2xl:px-0">
+          <h2 className={`title-page ${poppins.className}`}>About Us</h2>
+            <div className="grid grid-cols-3 gap-y-8 gap-x-4">
+              {blog.map((item, i) => {
+                return (
+                  <div key={i} className="item-blog">
+                    <div className="thumb w-100 h-[261px] relative mb-[20px]">
+                      <Image src={item.thumbnail} alt="thumbnail" fill/>
+                    </div>
+                    <h2 className={`text-[30px] font-bold ${poppins.className} leading-[35px] mb-4`}>{item.title}</h2>
+                    <p className='text-[16px]'>{item.desc}</p>
+                    <div className='text-right mt-4'>
+                      <Link href={item.url} className='underline'> Read More </Link>
+                    </div>
+                  </div>
+                )})}
+            </div>
+
+          <div className="mt-8">
+              <ul className='pagination flex justify-center items-center'>
+                <li className=''>
+                  <Link className='' href="#">1</Link>
+                </li>
+                <li>
+                  <Link href="#">2</Link>
+                </li>
+                <li>
+                  <Link href="#" className='text-black font-bold'>3</Link>
+                </li>
+                <li>
+                  <Link href="#">4</Link>
+                </li>
+                <li>
+                  <Link href="#">5</Link>
+                </li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ data/blog.js
@@ -0,0 +1,38 @@
+export const blog = [
+    {
+        title:'Lorem ipsum dolor sit amet, consectetur',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in euismod ipsum, sagittis condimentum ex. Sed aliquam tellus malesuada...',
+        url:'#',
+        thumbnail:'/blog/image-kucing.png'
+    },
+    {
+        title:'Lorem ipsum dolor sit amet, consectetur',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in euismod ipsum, sagittis condimentum ex. Sed aliquam tellus malesuada...',
+        url:'#',
+        thumbnail:'/blog/image-kucing.png'
+    },
+    {
+        title:'Lorem ipsum dolor sit amet, consectetur',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in euismod ipsum, sagittis condimentum ex. Sed aliquam tellus malesuada...',
+        url:'#',
+        thumbnail:'/blog/image-kucing.png'
+    },
+    {
+        title:'Lorem ipsum dolor sit amet, consectetur',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in euismod ipsum, sagittis condimentum ex. Sed aliquam tellus malesuada...',
+        url:'#',
+        thumbnail:'/blog/image-kucing.png'
+    },
+    {
+        title:'Lorem ipsum dolor sit amet, consectetur',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in euismod ipsum, sagittis condimentum ex. Sed aliquam tellus malesuada...',
+        url:'#',
+        thumbnail:'/blog/image-kucing.png'
+    },
+    {
+        title:'Lorem ipsum dolor sit amet, consectetur',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in euismod ipsum, sagittis condimentum ex. Sed aliquam tellus malesuada...',
+        url:'#',
+        thumbnail:'/blog/image-kucing.png'
+    }
+]
